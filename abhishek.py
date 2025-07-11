32ecf915d61fc0ede62855c11007926ecfeb3461
agent1411 <66353434+agent1411@users.noreply.github.com>
2025-07-11 15:01:25 +0530

    Create abhishek.py

---
+++ abhishek.py
@@ -0,0 +1,15 @@
+import numpy as np
+import streamlit as st
+import pickle
+
+model = pickle.load(open('Abhishek.mdl','rb'))
+st.title("Customer Segmentation by Abhishek")
+
+age = float(st.text_input("Enter Age: ","18"))
+salary = float(st.text_input("Enter Salary: ","10000"))
+
+featureInput = np.array([[age,salary]])
+
+sal = model.predict(featureInput)
+
+st.write(f'Hello, *World!* :sunglasses: . Customer Group : {sal} ')
